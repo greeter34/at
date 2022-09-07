306bca24bcc0455320f14bc6d853e1a353a62f7e
greeter34 <100347697+greeter34@users.noreply.github.com>
2022-09-07 02:36:11 -0300

    Room generation code
    Added code to generate one room in upper left quadrant of map

--- map.c
+++ map.c
@@ -85,14 +85,20 @@ void redraw_screen() { //this function should be called, rather than one of the 
 }
 
 void generate_level () {
+    struct Coord {
+        unsigned int x;
+        unsigned int y;
+    };
     if (been_here[hero.z]) {
         panic(9); //panic. we have been here before, so this function should not have been called and continuing may disrupt the game
     }
-    int x = 0, y = 0, i_x = 0, i_y = 0, chars = 0, random_int = 0;
+    int i = 0, x = 0, y = 0, i_x = 0, i_y = 0, wall_length = 0, wall_height = 0;
+    struct Coord upper_lc, upper_rc, lower_lc, lower_rc;
     getmaxyx(map, y, x);
     for (i_y = 0; i_y < y; i_y++) {
         for (i_x = 0; i_x < x; i_x++) {
-            random_int = roll(1, 1000);
+            maps[hero.z][i_x][i_y] = ' '; //blank the map to start
+            /*random_int = roll(1, 1000);
             update_windows();
             //following code is proof of concept. i plan extensive rewrites to this
             if (random_int > 300 && random_int < 997) {
@@ -111,6 +117,41 @@ void generate_level () {
             }
             else {
                 maps[hero.z][i_x][i_y] = ' ';
+            }*/
+        }
+    }
+
+    //establish room corners
+    upper_lc.x = roll(1, (x / 4));
+    upper_lc.y = roll(1, (y / 4));
+    upper_rc.x = (upper_lc.x + (roll(1, 10) + 3));
+    upper_rc.y = upper_lc.y;
+    lower_lc.x = upper_lc.x;
+    lower_lc.y = (upper_lc.y + (roll(1, 10) + 3));
+    lower_rc.x = upper_rc.x;
+    lower_rc.y = lower_lc.y;
+    //assign room corner values into array
+    maps[hero.z][upper_lc.x][upper_lc.y] = '3';
+    maps[hero.z][upper_rc.x][upper_rc.y] = '4';
+    maps[hero.z][lower_lc.x][lower_lc.y] = '5';
+    maps[hero.z][lower_rc.x][lower_rc.y] = '6';
+    //fill in horizontal walls
+    for (i = (upper_lc.x + 1); i < upper_rc.x; i++) {
+        maps[hero.z][i][upper_lc.y] = '2';
+        maps[hero.z][i][lower_lc.y] = '2';
+    }
+    //fill in vertical walls
+    for (i = (upper_lc.y + 1); i < lower_lc.y; i++) {
+        maps[hero.z][upper_lc.x][i] = '1';
+        maps[hero.z][upper_rc.x][i] = '1';
+    }
+    for (i_y = upper_lc.y; i_y < lower_rc.y; i_y++) {
+        for (i_x = upper_lc.x; i_x < upper_rc.x; i_x++) {
+            if (maps[hero.z][i_x][i_y] != ' ') {
+                continue;
+            }
+            else {
+                maps[hero.z][i_x][i_y] = '.';
             }
         }
     }
