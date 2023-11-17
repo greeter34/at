0963c5a13ebe1273240a0ca2eb0903bf79f60d3a
grin-ton <giacomo@grinton.dev>
2023-11-17 21:28:14 +0100

    Some more structs for the game (locations, coords, world,

--- game.c
+++ game.c
@@ -18,3 +18,36 @@ destroy_player (struct Player *player)
 {
   free(player);
 }
+
+struct Map
+*generate_map ()
+{
+  struct Map *map = calloc(1, sizeof(struct Map));
+  return map;
+}
+
+void
+destroy_map (struct Map *map)
+{
+  /* TODO: free the `locations'  as well, if it isn't null. */
+  free(map);
+}
+
+struct World
+*generate_world ()
+{
+  struct World *world = calloc(1, sizeof(struct World));
+
+  world->map    = generate_map();
+  world->player = generate_player();
+
+  return world;
+}
+
+void
+destroy_world (struct World *world)
+{
+  destroy_player(world->player);
+  destroy_map(world->map);
+  free(world);
+}
